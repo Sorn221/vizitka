0441f2712389cfb6d265ad95ee529e423d270101
razewithlove <90218991+Sorn221@users.noreply.github.com>
2024-08-23 06:44:53 +0300

    Update index.php

--- vizitka-api/index.php
+++ vizitka-api/index.php
@@ -7,7 +7,6 @@ require __DIR__ . "/vendor/autoload.php";
 
 require "init.php";
 
-header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type");
